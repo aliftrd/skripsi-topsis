ef63202386b61ff4d64f69bd9f4b55f4391dd3cf
Alif Triadi <aliftriadi.gaming@gmail.com>
2025-04-17 01:53:23 +0700

    170425: reformat export columns

--- resources/views/export/calculation.blade.php
+++ resources/views/export/calculation.blade.php
@@ -1,18 +1,32 @@
 <table>
     <thead>
         <tr>
-            <th scope="col">{{ __('calculation.columns.no') }}</th>
-            <th scope="col">{{ __('calculation.columns.name') }}</th>
-            <th scope="col">P</th>
+            <th bgcolor="#5B9BD5" style="color: white;">{{ __('citizen.columns.code') }}</th>
+            <th bgcolor="#5B9BD5" style="color: white;">{{ __('citizen.columns.name') }}</th>
+            <th bgcolor="#5B9BD5" style="color: white;">{{ __('citizen.columns.nik') }}</th>
+            <th bgcolor="#5B9BD5" style="color: white;">{{ __('citizen.columns.rt') }}</th>
+            <th bgcolor="#5B9BD5" style="color: white;">{{ __('citizen.columns.rw') }}</th>
+            <th bgcolor="#5B9BD5" style="color: white;">{{ __('citizen.columns.province') }}</th>
+            <th bgcolor="#5B9BD5" style="color: white;">{{ __('citizen.columns.district') }}</th>
+            <th bgcolor="#5B9BD5" style="color: white;">{{ __('citizen.columns.subdistrict') }}</th>
+            <th bgcolor="#5B9BD5" style="color: white;">{{ __('citizen.columns.village') }}</th>
+            <th bgcolor="#5B9BD5" style="color: white;">{{ __('citizen.columns.address') }}</th>
+            <th bgcolor="#5B9BD5" style="color: white;">P</th>
         </tr>
     </thead>
     <tbody>
         @foreach ($citizenRanks as $citizen)
             <tr>
-                <td>
-                    {{ $loop->index + 1 }}
-                </td>
+                <td>{{ $citizen->code }}</td>
                 <td>{{ $citizen->name }}</td>
+                <td>{{ $citizen->nik }}</td>
+                <td>{{ $citizen->rt }}</td>
+                <td>{{ $citizen->rw }}</td>
+                <td>{{ $citizen->province }}</td>
+                <td>{{ $citizen->district }}</td>
+                <td>{{ $citizen->subdistrict }}</td>
+                <td>{{ $citizen->village }}</td>
+                <td>{{ $citizen->address }}</td>
                 <td>{{ $preferenceValue[$citizen->code] ?? '-' }}</td>
             </tr>
         @endforeach
